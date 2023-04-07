1c225a42c4dba49d41babb5a7e07a0c0c2b7df4c
Adrian Antczak <adek24680@gmail.com>
2023-04-07 13:05:50 +0200

    4.1. Fix class names

---
+++ src/main/java/edu/kis/vh/nursery/factory/RhymersFactory.java
@@ -0,0 +1,15 @@
+package edu.kis.vh.nursery.factory;
+
+import edu.kis.vh.nursery.DefaultRhymer;
+
+public interface RhymersFactory {
+
+    public DefaultRhymer GetStandardRhymer();
+
+    public DefaultRhymer GetFalseRhymer();
+
+    public DefaultRhymer GetFIFORhymer();
+
+    public DefaultRhymer GetHanoiRhymer();
+
+}
