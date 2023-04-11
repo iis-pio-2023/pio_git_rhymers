5d4de0925d615f251c21f7cdb205b5d0dd08f573
Anna Sala <anna.sala056@gmail.com>
2023-04-11 14:51:42 +0200

    4.1 Poprawa nazw klas

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -1,7 +1,10 @@
 package edu.kis.vh.nursery;
 
+<<<<<<< HEAD
 import edu.kis.vh.nursery.DefaultCountingOutRhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
+=======
+>>>>>>> 0b69c0a (4.1 Poprawa nazw klas)
 import edu.kis.vh.nursery.factory.DefaultRhymersFactory;
 import edu.kis.vh.nursery.factory.Rhymersfactory;
 
@@ -9,8 +12,8 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
-        DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.GetFalseRhymer(),
+
+        DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
         
         for (int i = 1; i < 15; i++)
