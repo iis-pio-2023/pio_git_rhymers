4dabb47e2a4201a9f0498b64971d9dfef5d6ef67
Olek Golus <olekgolus02@gmail.com>
2023-04-13 22:00:18 +0200

    4.2 Fix method names

--- src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
+++ src/main/java/edu/kis/vh/nursery/factory/DefaultRhymersFactory.java
@@ -4,25 +4,25 @@ import edu.kis.vh.nursery.DefaultRhymer;
 import edu.kis.vh.nursery.FIFORhymer;
 import edu.kis.vh.nursery.HanoiRhymer;
 
-public class DefaultRhymersFactory implements Rhymersfactory {
+public class DefaultRhymersFactory implements RhymersFactory {
 
     @Override
-    public DefaultRhymer GetStandardRhymer() {
+    public DefaultRhymer getStandardRhymer() {
         return new DefaultRhymer();
     }
 
     @Override
-    public DefaultRhymer GetFalseRhymer() {
+    public DefaultRhymer getFalseRhymer() {
         return new DefaultRhymer();
     }
 
     @Override
-    public DefaultRhymer GetFIFORhymer() {
+    public DefaultRhymer getFIFORhymer() {
         return new FIFORhymer();
     }
 
     @Override
-    public DefaultRhymer GetHanoiRhymer() {
+    public DefaultRhymer getHanoiRhymer() {
         return new HanoiRhymer();
     }
 
