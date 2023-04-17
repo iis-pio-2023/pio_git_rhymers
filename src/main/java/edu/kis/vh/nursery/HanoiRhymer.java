4ab7ec0ea74841c41adabf64f6cbf5ce27e103f9
WiktoriaKrzyzanska <97694545+WiktoriaKrzyzanska@users.noreply.github.com>
2023-04-17 19:56:07 +0200

    8. Dodanie metody Override tam gdzie można

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -8,7 +8,7 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
         return totalRejected;
     }
 
-    public final void countIn(int in) {
+    @Override public final void countIn(int in) {
         if (!callCheck() && in > peekaboo())
             totalRejected++;
         else
