2005e3f6b6a20e3cd5951b3520772ed9ddeab67e
AverageDanteEnjoyer <236687@edu.p.lodz.pl>
2023-04-18 21:44:27 +0200

    9. Zmiana deklaracji atrybutów pod kątem widoczności

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -6,12 +6,11 @@ public class DefaultCountingOutRhymer {
     public static final int INT1 = 12;
     private final int[] numbers = new int[INT1];
 
-    public int total = INT;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
-
     }
 
     public boolean callCheck() {
