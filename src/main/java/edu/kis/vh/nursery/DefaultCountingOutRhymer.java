c1953b416ea4ff926800f376d3e33cd0f834a582
Kacper Zatoński <240844@edu.p.lodz.pl>
2023-04-19 15:02:08 +0200

    Punkt 6 dzial 2, Zmiana deklaracji stalych.

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,34 +1,34 @@
 package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
-
-    private int[] numbers = new int[12];
-
+    private final int STACK_SIZE = 12;
+    private final int[] NUMBERS = new int[STACK_SIZE];
     public int total = -1;
+    private final int EMPTY_STACK = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            numbers[++total] = in;
+            NUMBERS[++total] = in;
     }
 
         public boolean callCheck() {
-            return total == -1;
+            return total == EMPTY_STACK;
         }
         
             public boolean isFull() {
-                return total == 11;
+                return total == STACK_SIZE -1;
             }
         
                 protected int peekaboo() {
                     if (callCheck())
                         return -1;
-                    return numbers[total];
+                    return NUMBERS[total];
                 }
             
                     public int countOut() {
                         if (callCheck())
                             return -1;
-                        return numbers[total--];
+                        return NUMBERS[total--];
                     }
 
 }
