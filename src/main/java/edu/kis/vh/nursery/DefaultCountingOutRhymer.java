2a49ed7eca38c228fdf0d239f1f83e89b7d45641
Mwteusz <240759@edu.p.lodz.pl>
2023-04-19 15:18:24 +0200

    7. ustawienie final przy atrybutach klas

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer {
 
     private final int EMPTY = -1;
     private final int SIZE = 12;
-    private int[] numbers = new int[SIZE];
+    private final int[] numbers = new int[SIZE];
 
     public int total = -1;
 
