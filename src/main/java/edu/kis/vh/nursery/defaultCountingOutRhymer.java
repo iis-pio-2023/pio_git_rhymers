e693ad597dab17dec4f0163d52bc1544e7344824
Marta Ambroziak <97179185+Marta-Ambroziak@users.noreply.github.com>
2023-04-19 13:18:00 +0200

    6.1 Zmiana nazw zmiennych stalych, dodanie stalej zmiennej DEFAULT oraz INDEX

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -3,34 +3,35 @@ package edu.kis.vh.nursery;
 public class defaultCountingOutRhymer {
 
     public static final int MAX_SIZE = 12;
-    public static final int IS_EMPTY = -1;
+    public static final int INDEX = MAX_SIZE - 1;
+    public static final int DEFAULT = -1;
     private final int[] NUMBERS = new int[MAX_SIZE];
 
-    public int total = -1;
+    public int totalNumbers = -1;
 
     public void countIn(int in) {
         if (!isFull())
-            NUMBERS[++total] = in;
+            NUMBERS[++totalNumbers] = in;
     }
 
         public boolean callCheck() {
-            return total == IS_EMPTY;
+            return totalNumbers == DEFAULT;
         }
         
             public boolean isFull() {
-                return total == MAX_SIZE - 1;
+                return totalNumbers == INDEX;
             }
         
                 protected int peekaboo() {
                     if (callCheck())
-                        return -1;
-                    return NUMBERS[total];
+                        return DEFAULT;
+                    return NUMBERS[totalNumbers];
                 }
             
                     public int countOut() {
                         if (callCheck())
-                            return -1;
-                        return NUMBERS[total--];
+                            return DEFAULT;
+                        return NUMBERS[totalNumbers--];
                     }
 
 }
