806dbd0f17f120c1ca1a50be8389f765223712d3
Wikcis <wiktor.ciszewski7734@gmail.com>
2023-04-20 11:27:28 +0200

    6. Zmiana literałów stałymi

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,32 +2,33 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int TAB_CAPACITY = 12;
+    public static final int STARTING_VALUE = -1;
+    private int[] numbers = new int[TAB_CAPACITY];
 
-    public int total = -1;
+    public int total = STARTING_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
-
         public boolean callCheck() {
-            return total == -1;
+            return total == STARTING_VALUE;
         }
-        
+
             public boolean isFull() {
-                return total == 11;
+                return total == TAB_CAPACITY-1;
             }
         
                 protected int peekaboo() {
                     if (callCheck())
-                        return -1;
+                        return STARTING_VALUE;
                     return numbers[total];
                 }
             
                     public int countOut() {
                         if (callCheck())
-                            return -1;
+                            return STARTING_VALUE;
                         return numbers[total--];
                     }
 
