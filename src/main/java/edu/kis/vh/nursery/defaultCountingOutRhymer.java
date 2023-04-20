8f3186f64b4637ea39a893f95b29bb0c606ea35c
Michal Dabkowski <michal.dabkowski321@gmail.com>
2023-04-20 11:25:21 +0200

    Extract variables to constants

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,33 +2,36 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    private static final int SIZE = 12;
+    public static final int INITIAL_VALUE = -1;
+    private static final int TOTAL = -1;
 
-    public int total = -1;
+    private int[] NUMBERS = new int[SIZE];
+
+    public int total = TOTAL;
 
     public void countIn(int in) {
         if (!isFull())
-            numbers[++total] = in;
+            NUMBERS[++total] = in;
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == INITIAL_VALUE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == SIZE - 1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
-        return numbers[total];
+            return INITIAL_VALUE;
+        return NUMBERS[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
-        return numbers[total--];
+            return INITIAL_VALUE;
+        return NUMBERS[total--];
     }
-
 }
