54d4a47cdf4147407a4f70673b8e43a3c2d73967
Hubert Cywka <hcywka@gmail.com>
2023-04-21 22:20:49 +0200

    11. Ograniczenie widoczności atrybutów i dodanie getterów i setterów  w klasie Node

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,11 +2,35 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    protected int value;
-    protected Node prevNode, nextNode;
+    private int value;
+    private Node prevNode;
+    private Node nextNode;
 
     public Node(int i) {
         value = i;
     }
 
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+
+    public Node getPrevNode() {
+        return prevNode;
+    }
+
+    public void setPrevNode(Node prevNode) {
+        this.prevNode = prevNode;
+    }
+
+    public Node getNextNode() {
+        return nextNode;
+    }
+
+    public void setNextNode(Node nextNode) {
+        this.nextNode = nextNode;
+    }
 }
