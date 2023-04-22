9710c4ce81e4f1557e8ef49b5873fa0f3b626409
Hidennnn <240629@edu.p.lodz.pl>
2023-04-22 18:14:36 +0200

    6. Literały zmienione na stałe

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,15 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int NUMBERS_SIZE = 12;
+    public static final int TOTAL_START_VALUE = -1;
+    public static final int EMPTY_TABLE = -1;
+    public static final int FULL_TABLE = 11;
+    public static final int PEEKABOO_WITH_EMPTY_TABLE = -1;
+    public static final int COUNT_OUT_WITH_FULL_TABLE = -1;
+    private int[] numbers = new int[NUMBERS_SIZE];
 
-    public int total = -1;
+    public int total = TOTAL_START_VALUE;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +18,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == EMPTY_TABLE;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == FULL_TABLE;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return PEEKABOO_WITH_EMPTY_TABLE;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return COUNT_OUT_WITH_FULL_TABLE;
         return numbers[total--];
     }
 
