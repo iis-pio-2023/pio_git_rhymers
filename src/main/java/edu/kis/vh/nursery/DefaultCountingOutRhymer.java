a1662dbc5b3de135eaf1d38ccadcb1ea6dd075fc
Maksymilian Pietras <240774@edu.p.lodz.pl>
2023-04-22 12:17:06 +0200

    Accessory i hermetyzacja

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer {
 
     private int[] numbers = new int[12];
 
-    public int total = -1;
+    private int total = -1;
 
     public void countIn(int in) {
         if (!isFull())
@@ -29,6 +29,10 @@ public class DefaultCountingOutRhymer {
         if (callCheck())
             return -1;
         return numbers[total--];
+    }
+
+    public int getTotal() {
+        return total;
     }
 
 }
