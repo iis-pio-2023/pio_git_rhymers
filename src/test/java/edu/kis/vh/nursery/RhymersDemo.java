15cb39d97b2a0c47237c95a9071083d8466983f8
Emil Sarba <240793@edu.p.lodz.pl>
2023-04-22 21:00:57 +0200

    6. poprawa literałów

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -5,19 +5,23 @@ import edu.kis.vh.nursery.factory.Rhymersfactory;
 
 class RhymersDemo {
 
+    public static final int MAX_VALUE = 15;
+    public static final int RHYMERS_SIZE = 3;
+    public static final int MAX_RANDOMISATION_VALUE = 20;
+
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
         
         DefaultCountingOutRhymer[] rhymers = { factory.getStandardRhymer(), factory.getFalseRhymer(),
                 factory.getFIFORhymer(), factory.getHanoiRhymer()};
         
-        for (int i = 1; i < 15; i++)
-            for (int j = 0; j < 3; j++)
+        for (int i = 1; i < MAX_VALUE; i++)
+            for (int j = 0; j < RHYMERS_SIZE; j++)
                 rhymers[j].countIn(i);
         
         java.util.Random rn = new java.util.Random();
-        for (int i = 1; i < 15; i++)
-            rhymers[3].countIn(rn.nextInt(20));
+        for (int i = 1; i < MAX_VALUE; i++)
+            rhymers[RHYMERS_SIZE].countIn(rn.nextInt(MAX_RANDOMISATION_VALUE));
         
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
@@ -26,7 +30,7 @@ class RhymersDemo {
         }
         
         System.out.println("total rejected is "
-                + ((HanoiRhymer) rhymers[3]).reportRejected());
+                + ((HanoiRhymer) rhymers[RHYMERS_SIZE]).reportRejected());
         
     }
     
