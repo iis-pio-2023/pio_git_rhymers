d78f5d29c4aff78f02866143a7732216f6b237c1
jaqubm <jaqobgm@gmail.com>
2023-04-23 13:00:10 +0200

    #12 Usunięcie nieużywanych setterów

--- src/main/java/edu/kis/vh/nursery/FifoRhymer.java
+++ src/main/java/edu/kis/vh/nursery/FifoRhymer.java
@@ -20,8 +20,4 @@ public class FifoRhymer extends DefaultCountingOutRhymer {
     public DefaultCountingOutRhymer getTemp() {
         return temp;
     }
-
-    public void setTemp(DefaultCountingOutRhymer temp) {
-        this.temp = temp;
-    }
 }
