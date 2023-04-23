557990d09935a92499ce3625070f061500628bff
Jakub Rychlik <240788@edu.p.lodz.pl>
2023-04-23 13:52:03 +0200

    6. Zastapienie literalow stalymi

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -2,9 +2,11 @@ package edu.kis.vh.nursery;
 
 public class DefaultCountingOutRhymer {
 
-    private int[] numbers = new int[12];
+    public static final int MAX_NUMBERS = 12;
+    public static final int TOTAL_INIT_VAL = -1;
+    private int[] numbers = new int[MAX_NUMBERS];
 
-    private int total = -1;
+    private int total = TOTAL_INIT_VAL;
 
     public void countIn(int in) {
         if (!isFull())
@@ -12,22 +14,22 @@ public class DefaultCountingOutRhymer {
     }
 
     public boolean callCheck() {
-        return total == -1;
+        return total == TOTAL_INIT_VAL;
     }
 
     public boolean isFull() {
-        return total == 11;
+        return total == MAX_NUMBERS-1;
     }
 
     protected int peekaboo() {
         if (callCheck())
-            return -1;
+            return TOTAL_INIT_VAL;
         return numbers[total];
     }
 
     public int countOut() {
         if (callCheck())
-            return -1;
+            return TOTAL_INIT_VAL;
         return numbers[total--];
     }
 
--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -2,7 +2,8 @@ package edu.kis.vh.nursery;
 
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
-    private int totalRejected = 0;
+    public static final int TOTAL_INIT_VAL = 0;
+    private int totalRejected = TOTAL_INIT_VAL;
 
     public int reportRejected() {
         return totalRejected;
