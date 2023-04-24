6a728be2f4c57f4e1cadd49ef7d95b0933dd6898
Joki004 <101185519+Joki004@users.noreply.github.com>
2023-04-24 15:45:44 +0200

    7. ustawienie modyfikator final

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer {
 
     public static final int ERROR = -1;
     public static final int MAX_SIZE = 12;
-    private int[] numbers = new int[MAX_SIZE];
+    private final int[] numbers = new int[MAX_SIZE];
 
     public int total = ERROR;
 
