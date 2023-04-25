7d4100b4f1ba7d42104cc0396360712792c8846a
Piotr Jurek <240693@edu.p.lodz.pl>
2023-04-25 19:03:59 +0200

    11. Dokonanie hermetyzacji nieprywatnych atrybutów

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -10,9 +10,9 @@ public class IntLinkedList {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().setPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -27,14 +27,14 @@ public class IntLinkedList {
     public int top() {
         if (isEmpty())
             return IF_EMPTY;
-        return last.VALUE;
+        return last.getVALUE();
     }
 
     public int pop() {
         if (isEmpty())
             return IF_EMPTY;
-        int ret = last.VALUE;
-        last = last.prev;
+        int ret = last.getVALUE();
+        last = last.getPrev();
         return ret;
     }
 
