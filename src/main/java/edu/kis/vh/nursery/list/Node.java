2dc70b79dfd2a30b68c45260be5174a2246cf7a0
Kacper Michalec <240751@edu.p.lodz.pl>
2023-04-25 19:03:56 +0200

    11. Hermetyzacja nieprywatnych atrybutow

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,11 +2,31 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    protected final int VALUE;
-    protected Node prev, next;
+    private final int VALUE;
+    private Node prev;
+    private Node next;
 
     public Node(int i) {
         VALUE = i;
     }
 
+    public int getVALUE() {
+        return VALUE;
+    }
+
+    public Node getPrev() {
+        return prev;
+    }
+
+    public void setPrev(Node prev) {
+        this.prev = prev;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
 }
