8f66d8f5503fbe5be88a9f0a925231d2f6df0130
Hubert Cywka <hcywka@gmail.com>
2023-04-27 13:42:26 +0200

    17. Dodanie dokumentacji do klasy DefaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * @brief basic rhymer, last inserted value is returned first
+ */
 public class DefaultCountingOutRhymer {
 
     private static final int INITIAL_TOTAL = -1;
@@ -9,25 +12,45 @@ public class DefaultCountingOutRhymer {
     private final int[] numbers = new int[NUMBERS_AMOUNT];
     private int total = INITIAL_TOTAL;
 
+    /**
+     * @brief adds value to rhymer numbers
+     * @param in - value to add
+     */
     public void countIn(int in) {
         if (!isFull())
             numbers[++total] = in;
     }
 
+    /**
+     * @brief checks if rhymers is empty
+     * @return true if rhymer is empty, else returns false
+     */
     public boolean callCheck() {
         return total == INITIAL_TOTAL;
     }
 
+    /**
+     * @brief checks if rhymers is full
+     * @return true if rhymer is full, else returns false
+     */
     public boolean isFull() {
         return total == NUMBERS_AMOUNT + INITIAL_TOTAL;
     }
 
+    /**
+     * @brief gets latest inserted value
+     * @return ERROR when rhymer is empty, or latest inserted value
+     */
     protected int peekaboo() {
         if (callCheck())
             return ERROR;
         return numbers[total];
     }
 
+    /**
+     * @brief gets latest inserted value and removes it from rhymer
+     * @return ERROR when rhymer is empty, or latest inserted value
+     */
     public int countOut() {
         if (callCheck())
             return ERROR;
