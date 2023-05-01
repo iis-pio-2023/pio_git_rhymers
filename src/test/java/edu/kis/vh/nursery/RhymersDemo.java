f92c38d6a7ab9cae4ae0a9c303f38297ca496208
Piotr Jasiński <piotr.jasinski2002@gmail.com>
2023-05-01 11:59:40 +0200

    13. Uporządkowanie aplikacji demo RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -12,27 +12,30 @@ class RhymersDemo {
 
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
-        
+
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(Rhymersfactory factory){
         defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
-        
+
         for (int i = 1; i < COUNT_VALUE; i++)
             for (int j = 0; j < 3; j++)
                 rhymers[j].countIn(i);
-        
+
         java.util.Random rn = new java.util.Random();
         for (int i = 1; i < COUNT_VALUE; i++)
             rhymers[3].countIn(rn.nextInt(BOUND));
-        
+
         for (int i = 0; i < rhymers.length; i++) {
             while (!rhymers[i].callCheck())
                 System.out.print(rhymers[i].countOut() + "  ");
             System.out.println();
         }
-        
+
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-        
+
     }
-    
 }
