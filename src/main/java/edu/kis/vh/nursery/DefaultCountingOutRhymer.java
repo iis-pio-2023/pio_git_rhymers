3490c545c4b564dc9b9be8b625856bc51f1ecb01
kaszczu02 <240701@edu.p.lodz.pl>
2023-05-07 18:08:52 +0200

    6.Refaktoryzacja zmiennych

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -4,7 +4,7 @@ public class DefaultCountingOutRhymer
 {
 
     public static final int MAX_SIZE = 12;
-    public static final int EMPTY_RHYMER_INDICATOR = DEFAULT_VALUE;
+    public static final int EMPTY_RHYMER_INDICATOR = -1;
     public static final int DEFAULT_VALUE = -1;
     private int[] numbers = new int[MAX_SIZE];
 
