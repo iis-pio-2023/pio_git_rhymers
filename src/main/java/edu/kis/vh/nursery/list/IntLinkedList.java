af8b23d9f72491bfface9fc5c7b034c388ce2f7c
Matenar <240736@edu.p.lodz.pl>
2023-05-09 00:47:05 +0200

    11. Poprawa Hermetyzacja nieprywatnch atrybutów

--- src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
+++ src/main/java/edu/kis/vh/nursery/list/IntLinkedList.java
@@ -1,17 +1,18 @@
 package edu.kis.vh.nursery.list;
 
+
 public class IntLinkedList {
 
-    Node last;
-    int i;
+    private Node last;
+    private int i;
 
     public void push(int i) {
         if (last == null)
             last = new Node(i);
         else {
-            last.next = new Node(i);
-            last.next.prev = last;
-            last = last.next;
+            last.setNext(new Node(i));
+            last.getNext().getPrev(last);
+            last = last.getNext();
         }
     }
 
@@ -26,15 +27,33 @@ public class IntLinkedList {
     public int top() {
         if (isEmpty())
             return -1;
-        return last.value;
+        return last.getValue();
+    }
+
+    public int getI() {
+        return i;
+    }
+
+    public void setI(int i){
+        this.i=i;
+    }
+
+    public void setLast(Node last) {
+        this.last = last;
+    }
+
+    public Node getLast() {
+        return last;
     }
 
     public int pop() {
         if (isEmpty())
             return -1;
-        int ret = last.value;
-        last = last.prev;
+        int ret = last.getValue();
+        last = last.getPrev(last);
         return ret;
     }
+
+
 
 }
