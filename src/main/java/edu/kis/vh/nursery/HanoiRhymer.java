7aedebd839bd41b8ed3a9c7db696089144041bf9
Damian Mendel <mendeldamian01@gmail.com>
2023-05-09 16:45:50 +0200

    generate docs for HanoiRhymer

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -1,9 +1,19 @@
 package edu.kis.vh.nursery;
 
+/**
+ * HanoiRhymer is a class that extends DefaultCountingOutRhymer and overrides countIn method.
+ */
 public class HanoiRhymer extends DefaultCountingOutRhymer {
 
+    /**
+     * totalRejected is a counter of rejected numbers.
+     */
     private int totalRejected = 0;
 
+    /**
+     * reportRejected returns totalRejected.
+     * @return totalRejected
+     */
     public int reportRejected() {
         return totalRejected;
     }
