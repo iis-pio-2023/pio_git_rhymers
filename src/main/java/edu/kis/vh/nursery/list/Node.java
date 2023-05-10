e5cbbaec0688a83ec60cb85b1d066efc1f2dfb48
Obraiel <konrad.marcinnn@gmail.com>
2023-05-10 15:08:39 +0200

    9.modyfikacja atrbutów klas

--- src/main/java/edu/kis/vh/nursery/list/Node.java
+++ src/main/java/edu/kis/vh/nursery/list/Node.java
@@ -2,8 +2,9 @@ package edu.kis.vh.nursery.list;
 
 public class Node {
 
-    public final int value;
-    public Node prev, next;
+    private final int value;
+    private Node prev;
+    private Node next;
 
     public Node(int i) {
         value = i;
