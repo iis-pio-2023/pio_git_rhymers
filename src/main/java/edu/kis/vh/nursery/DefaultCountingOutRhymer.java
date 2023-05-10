c51096c42d1f4a10c25f67ee20965e12c5695145
Obraiel <konrad.marcinnn@gmail.com>
2023-05-10 15:43:16 +0200

    10.generacja Getter i Setter dla pola total

--- src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/DefaultCountingOutRhymer.java
@@ -7,6 +7,15 @@ public class DefaultCountingOutRhymer {
 
     private final int[] NUMBERS = new int[STACK_SIZE];
     public final int EMPTY_STACK = -1;
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
+
     public int total = EMPTY_STACK;
 
 
