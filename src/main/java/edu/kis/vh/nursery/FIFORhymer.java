8d1b7da85659e6524d8e3e992bac6390bfce33e4
Mateusz <240699@edu.p.lodz.pl>
2023-05-10 23:55:17 +0200

    18. Dodanie TODO

--- src/main/java/edu/kis/vh/nursery/FIFORhymer.java
+++ src/main/java/edu/kis/vh/nursery/FIFORhymer.java
@@ -3,7 +3,7 @@ package edu.kis.vh.nursery;
 public class FIFORhymer extends defaultCountingOutRhymer {
 
     public defaultCountingOutRhymer temp = new defaultCountingOutRhymer();
-
+    // TODO: change name of field above
     @Override
     public int countOut() {
         while (!callCheck())
