b55f807088b91abe4b6b5f1138fa7e5fbf2f70e2
Mateusz <240699@edu.p.lodz.pl>
2023-05-10 23:17:01 +0200

    9. Usuniecie modyfikatorow public

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -2,7 +2,7 @@ package edu.kis.vh.nursery;
 
 public class defaultCountingOutRhymer {
 
-    private int[] NUMBERS = new int[12];
+    private final int[] NUMBERS = new int[12];
 
     public int total = -1;
 
@@ -11,11 +11,11 @@ public class defaultCountingOutRhymer {
             NUMBERS[++total] = in;
     }
 
-    public boolean callCheck() {
+    boolean callCheck() {
         return total == -1;
     }
 
-    public boolean isFull() {
+    boolean isFull() {
         return total == 11;
     }
 
