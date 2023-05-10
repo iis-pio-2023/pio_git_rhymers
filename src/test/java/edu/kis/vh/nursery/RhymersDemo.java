6859598661251f0d137620228069c98181604b01
MarcinFrejnagelPolibuda <243254@edu.p.lodz.pl>
2023-05-10 23:28:38 +0200

    13. Uporzadkowanie klasy RhymersDemo

--- src/test/java/edu/kis/vh/nursery/RhymersDemo.java
+++ src/test/java/edu/kis/vh/nursery/RhymersDemo.java
@@ -10,6 +10,10 @@ class RhymersDemo {
     public static void main(String[] args) {
         Rhymersfactory factory = new DefaultRhymersFactory();
 
+        testRhymers(factory);
+    }
+
+    private static void testRhymers(Rhymersfactory factory){
         defaultCountingOutRhymer[] rhymers = { factory.GetStandardRhymer(), factory.GetFalseRhymer(),
                 factory.GetFIFORhymer(), factory.GetHanoiRhymer()};
 
@@ -29,7 +33,5 @@ class RhymersDemo {
 
         System.out.println("total rejected is "
                 + ((HanoiRhymer) rhymers[3]).reportRejected());
-
     }
-
 }
