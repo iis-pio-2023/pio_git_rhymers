7e19e72a4cded30a521340c8ff293d7898ace265
MarcinFrejnagelPolibuda <243254@edu.p.lodz.pl>
2023-05-10 23:53:43 +0200

    17. Wygenerowanie szkieletu dokumentacji do klasy defaultCountingOutRhymer

--- src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
+++ src/main/java/edu/kis/vh/nursery/defaultCountingOutRhymer.java
@@ -1,5 +1,8 @@
 package edu.kis.vh.nursery;
 
+/**
+ * Class representing a nursery rhyme with counting elements.
+ */
 public class defaultCountingOutRhymer {
 
     public static final int NUMBERS_QUANTITY = 12;
@@ -11,30 +14,59 @@ public class defaultCountingOutRhymer {
 
     public int total = RHYMER_EMPTY_INDICATOR;
 
+    /**
+     * Adds an element to the rhyme.
+     *
+     * @param in element to add
+     */
     public void countIn(int in) {
         if (!isFull())
             NUMBERS[++total] = in;
     }
 
-
+    /**
+     * Returns the number of elements in the rhyme.
+     *
+     * @return the number of elements in the rhyme
+     */
     public int getTotal() {
         return total;
     }
-  
-    public boolean callCheck() {
-        return total == RHYMER_EMPTY_INDICATOR;
+
+    /**
+     * Checks if the rhyme is empty.
+     *
+     * @return true if the rhyme is empty; false otherwise
+     */
+    boolean callCheck() {
+        return total == -1;
     }
 
-    public boolean isFull() {
-        return total == RHYMER_FULL_INDICATOR;
+    /**
+     * Checks if the rhyme is full.
+     *
+     * @return true if the rhyme is full; false otherwise
+     */
+    boolean isFull() {
+        return total == 11;
     }
 
+    /**
+     * Returns the last added element of the rhyme.
+     *
+     * @return the last added element of the rhyme or -1 if the rhyme is empty
+     */
     protected int peekaboo() {
         if (callCheck())
             return RETURN_VALUE_RHYMER_EMPTY;
         return NUMBERS[total];
     }
 
+    /**
+     * Removes and returns the last added element of the rhyme.
+     *
+     * @return the last added element of the rhyme or -1 if the rhyme is empty
+     */
     int countOut() {
         if (callCheck())
             return RETURN_VALUE_RHYMER_EMPTY;
