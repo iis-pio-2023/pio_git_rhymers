538562c9807b51a093264e4d947f6e049093b208
Michal-Smiechowicz <240817@edu.p.lodz.pl>
2023-05-22 19:57:36 +0200

    17. Wygenerowanie szkieletu dokumentacji w HanoiRhymer.java

--- src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
+++ src/main/java/edu/kis/vh/nursery/HanoiRhymer.java
@@ -4,9 +4,18 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
 
     private int totalRejected = 0;
 
+    /**
+     *
+     * @return zwraca ilosc odrzuconych
+     */
     public int reportRejected() {
         return totalRejected;
     }
+
+    /**
+     *
+     * @param in sprawdza, czy in jest wieksze od peekaboo i zwieksza ilosc odrzuconych
+     */
     @Override
     public void countIn(int in) {
         if (!callCheck() && in > peekaboo())
@@ -15,6 +24,10 @@ public class HanoiRhymer extends DefaultCountingOutRhymer {
             super.countIn(in);
     }
 
+    /**
+     *
+     * @return getter
+     */
     public int getTotalRejected() {
         return totalRejected;
     }
